673867aef4cb5c049440c78daa14b0f0d0e62567
Rosa Truneh <rosa.truneh@gmail.com>
2021-05-19 00:00:59 -0500

    worked on color pallette and working on css

--- app.js
+++ app.js
@@ -1,1 +1,8 @@
 "use strict"
+
+const resutls = document.getElementById('item-clicks');
+const allItems = document.getElementById('all-items');
+const rightItemImg = document.getElementById("right_goat_img");
+const middleItemImg = document.getElementById("left_goat_img");
+const leftItemImg = document.getElementById("left_goat_p");
+const leftGoatP = document.getElementById("right_goat_p");
